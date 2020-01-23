2d75f71b9177d86cf758821304e1d7d4d3e5eac0
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 07:14:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 def long_planeteer_calls(array_of_calls)
    array_of_calls.any? { |call| call.length > 4 }
 end
-end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
