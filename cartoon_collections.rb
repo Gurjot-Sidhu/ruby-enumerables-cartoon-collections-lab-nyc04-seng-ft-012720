f284867c440a3012976875a373f0b5ffcb92c4e1
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 07:13:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+   array_of_calls.any? { |call| call.length > 4 }
+end
 end
 
 def find_the_cheese# code an argument here
