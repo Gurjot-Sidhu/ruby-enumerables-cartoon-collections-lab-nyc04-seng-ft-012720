ecad0589107728e26e11e5e4ac44d2a2de7e4f2d
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 07:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,11 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  
+  new = []
+  i = 0 
+  while i <array.length do
+    new[i] = array[i] + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
@@ -17,3 +21,48 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+var upper =[];
+function summonCaptainPlanet(planeteerCalls){
+  for(var i=0;i<planeteerCalls.length;i++){
+  upper[i]= planeteerCalls[i]+ "!";
+  upper = upper.map(function(x){return x.toUpperCase()})
+  }
+  return upper;
+}
+
+function longPlaneteerCalls(words) {
+  for(var i=0;i<words.length;i++){
+    if(words[i].length > 4){
+      return true;
+    }
+    if(words[i].length <=4){
+      return false;
+    }
+  }
+}
+
+function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+    if(foods[i] == "cheddar"){
+      return "cheddar";
+    }
+    if(foods[i] == "gouda"){
+      return "gouda";
+    }
+    if(foods[i] == "camembert"){
+      return "camembert";
+    }
+    if(foods[i] == " ")
+  }
+  return "no cheese!";
+}
+
+function wordsWithB(words){
+  var names = [];
+  for(var i=0;i <words.length;i++){
+    if(words[i].startsWith("b")){
+      names.push(words[i]);
+    }
+  }
+  return names;
+}
