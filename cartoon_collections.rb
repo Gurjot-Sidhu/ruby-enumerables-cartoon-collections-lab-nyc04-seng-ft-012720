79b17f4ba7a9e2f6c073f295c17ef030b116cc7a
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 07:15:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ def long_planeteer_calls(array_of_calls)
 end
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find do |cheese| 
+  food.find do |cheese| 
     cheese_types.include?(cheese) 
   end
 end
